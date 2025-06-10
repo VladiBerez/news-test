c0cfbb2d91eaa501fec60e6bc04fadac0adf9df7
Berezhnoi Vladimir <berezhnoivv@inbox.ru>
2025-06-10 17:36:05 +0300

    refact: added deploy

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const nextConfig = {
   },
   images: {
     domains: ['*'],
+    unoptimized: true
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/news-test' : '',
 };
 
 export default nextConfig; 
